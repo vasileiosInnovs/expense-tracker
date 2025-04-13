fe12cbb2d086f6974522b3700769b432a0fbc31b
hagee <hagee.ktr@outlook.com>
2025-04-13 21:09:48 +0300

    Generate table data row

--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-function Table(){
+function Table({ row }){
     return (
         <table>
+            <thead>
             <tr>
                 <th>Expense</th>
                 <th>Description</th>
@@ -11,6 +12,20 @@ function Table(){
                 <th>Date</th>
                 <th></th>
             </tr>
+            </thead>
+            <tbody>
+                {row.map((row) => {
+                    return ( <tr key={row.id}>
+                        <td>{row.expense}</td>
+                        <td>{row.description}</td>
+                        <td>{row.category}</td>
+                        <td>{row.amount}</td>
+                        <td>{row.date}</td>
+                        </tr>)
+                })}
+            </tbody>
         </table>
     )
-} 
+}
+
+export default Table;
