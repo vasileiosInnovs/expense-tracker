d7210057f910bd2b25b72b945619704c1a0e9aad
hagee <hagee.ktr@outlook.com>
2025-04-13 21:50:30 +0300

    Generate id for each new row created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Table from './Table';
 import AddExpense from './AddExpense';
+import { useState } from 'react';
 
 function App() {
+  const [rows, setRow] = useState([
+    {expense: "iPhone", description: "phone", category: "tech", amount: "133,100", date: "03/02/2015"},
+    {expense: "farm", description: "a piece of land", category: "real estate", amount: "1,500,000", date: "03/02/2015"},
+    {expense: "trip", description: "trip out", category: "vacation", amount: "2,000,000", date: "03/02/2015"}
+  ])
+  
   return (
     <div className="App">
       <header >
@@ -14,7 +22,9 @@ function App() {
       <section>
         <AddExpense />
       </section>
-      
+      <section>
+        <Table rows={rows}/>
+      </section>
     </div>
   );
 } 
--- src/Table.js
+++ src/Table.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
-function Table({ row }){
+function Table({ rows }){
+    const newId = rows.map(row => ({
+        id: crypto.randomUUID()
+    })) 
+    console.log(newId) 
     return (
         <table>
             <thead>
-            <tr>
-                <th>Expense</th>
-                <th>Description</th>
-                <th>Category</th>
-                <th>Amount</th>
-                <th>Date</th>
-                <th></th>
-            </tr>
+                <tr>
+                    <th>Expense</th>
+                    <th>Description</th>
+                    <th>Category</th>
+                    <th>Amount</th>
+                    <th>Date</th>
+                    <th></th>
+                </tr>
             </thead>
             <tbody>
-                {row.map((row) => {
-                    return ( <tr key={row.id}>
+                {rows.map((row) => {
+                    return ( <tr key={row.newId} >
                         <td>{row.expense}</td>
                         <td>{row.description}</td>
                         <td>{row.category}</td>
@@ -24,6 +28,7 @@ function Table({ row }){
                         </tr>)
                 })}
             </tbody>
+        
         </table>
     )
 }
